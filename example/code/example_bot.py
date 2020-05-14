47ce029f485de715e82ab446ad790e19f8eecb08
luckydonald <mac2019.git@luckydonald.de>
2020-05-14 22:10:12 +0200

    [telemenu] example: Copied over some examples from r2tg.

--- example/code/example_bot.py
+++ example/code/example_bot.py
@@ -9,13 +9,13 @@ logging.add_colored_handler(logger_name=None, level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 from os import environ
-from typing import List, Union, Type
+from typing import List, Union, Type, cast
 
 from flask import Flask
 from teleflask import Teleflask
 from telestate.contrib.simple import SimpleDictDriver
 
-from telemenu.menus import GotoMenu, Menu, RadioMenu, CheckboxMenu
+from telemenu.menus import GotoMenu, Menu, RadioMenu, CheckboxMenu, TextStrMenu
 from telemenu.machine import TeleMenuMachine
 from telemenu.buttons import ChangeMenuButton, BackButton, GotoButton, RadioButton, CheckboxButton
 
@@ -87,12 +87,12 @@ class TestCheckboxMenu(CheckboxMenu):
 
 
 @menus.register
-class MainMenu(GotoMenu):
+class TheMainMenu(GotoMenu):
     title = "Home"
     description = "This is the main menu. Jump wherever you want."
 
     def menus(self):
-        return [TestMenu, AnotherTestMenu, TestRadioMenu]
+        return [TestMenu, MainMenu, AnotherTestMenu, TestRadioMenu]
     # end def
 # end class
 
@@ -120,6 +120,94 @@ class TestRadioMenu(RadioMenu):
             RadioButton(title="Spike", default_selected=False, value='just_no'),
         ]
     # end def
+# end class
+
+
+@menus.register
+class MainMenu(GotoMenu):
+    title = 'Main Menu'
+    description = 'Please choose what you wanna do!'
+    menus = lambda: [NewBotSubreddit]
+# end def
+
+
+@menus.register
+class NewBotSubreddit(TextStrMenu):
+    title = "Subreddit"
+    description = "Heya, so you wanna create a new channel for all the reddit goodness?\nGreat! Let's get started! If you need to, you can abort at any time by sending /cancel.\n\nPlease send me the name of the reddit you wanna have in telegram."
+    done = lambda: NewBotSort
+    cancel = 'Cancel'
+    # todo: Validate the subreddit for an valid and existing one.
+# end class
+
+
+@menus.register
+class NewBotSort(RadioMenu):
+    title = "Post Mode"
+    cancel = 'Back'
+
+    @classmethod
+    def description(cls):
+        reddit_name = cls.data.menus[NewBotSubreddit.id]
+        reddit_link = f"https://reddit.com/r/{reddit_name}/"
+        return (
+            f'Got that. The <a href="{reddit_link}">{reddit_name}</a> subreddit it is.\n'
+            '\n'
+            'Do you want to post <code>hot</code>, <code>top</code> or <code>new</code> posts?\n'
+            'We recommend choosing <code>hot</code>.'
+        )
+    # end def
+
+    @classmethod
+    def done(cls):
+        """
+        The next variable is evaluated after all data processing is done,
+        so it can react to the input data.
+        In this case which element was chosen.
+        """
+        if cast(Data, cls.data).saved_data[NewBotSort.id] == 'top':
+            # top needs selecting a duration
+            return NewBotSelectHotTimeframe
+        # end if
+        # -> else -> skip directly to the media selection
+        return NewChannelContentType
+    # end def
+# end def
+
+
+@menus.register
+class NewBotSelectHotTimeframe(RadioMenu):
+    title = "Duration"
+    cancel = 'Back'
+
+    radiobuttons = [
+        RadioButton(title='All', value='all'),
+        RadioButton(title='Day', value='day'),
+        RadioButton(title='Hour', value='hour'),
+        RadioButton(title='Month', value='month'),
+        RadioButton(title='Week', value='week'),
+        RadioButton(title='Year', value='year'),
+    ]
+    done = lambda: NewChannelContentType
+# end def
+
+
+@menus.register
+class NewChannelContentType(CheckboxMenu):
+    title = "Media"
+    description = "Cool. Now which content types do you want to have posted?"
+    cancel = 'Back'
+
+    checkboxes = [
+        CheckboxButton(title='Text', value='text'),
+        CheckboxButton(title='Images', value='image'),
+        CheckboxButton(title='Albums', value='album'),
+        CheckboxButton(title='Links', value='link'),
+        CheckboxButton(title='Gifs', value='gif'),
+        CheckboxButton(title='Videos', value='video'),
+        CheckboxButton(title='Youtube', value='youtube'),
+    ]
+    done = MainMenu
 # end class
 
 
