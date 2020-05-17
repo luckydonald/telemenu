b006056b022863332621fc548851d2704088fb1b
luckydonald <mac2019.git@luckydonald.de>
2020-05-17 20:44:11 +0200

    [telemenu] Removed the menus from the unittests for now.

--- telemenu/test.py
+++ telemenu/test.py
@@ -3,24 +3,12 @@
 """ Usage:
 API_KEY = "......";  from pytgbot.bot import Bot; bot = Bot(API_KEY); from luckydonaldUtils.logger import logging; logger = logging.getLogger(__name__); logging.add_colored_handler(level=logging.DEBUG);import sys;sys.path.extend(['.../r2tg/code/r2tg/telemenu_dist']);from telemenu.test import telemenu, teleflask, TestCheckboxMenu, Menu, TestTextUrlMenu, TestRadioMenu, RegisterTestMenu, Update, Message, Chat
 """
-import re
-import inspect
 import unittest
 
-from typing import List, Union, cast
-
-from pytgbot.api_types.receivable.peer import Chat
-from teleflask import Teleflask
-from telestate import TeleState
+from luckydonaldUtils.decorators import classproperty
 from luckydonaldUtils.logger import logging
-from telestate.contrib.simple import SimpleDictDriver
-from pytgbot.api_types.receivable.updates import Update, Message
-from pytgbot.api_types.sendable.reply_markup import ForceReply
-from .data import MenuData, Data
-from .menus import Menu, GotoMenu, CheckboxMenu, RadioMenu, TextStrMenu, TextIntMenu, TextFloatMenu
-from .menus import TextPasswordMenu, TextEmailMenu, TextTelMenu, TextUrlMenu, UploadMenu
-from .buttons import GotoButton, DoneButton, CheckboxButton, RadioButton
-from .machine import TeleMenuMachine, TeleMenuInstancesItem
+from telemenu.data import Data
+from telemenu.menus import Menu
 
 from .somewhere import API_KEY
 
@@ -32,10 +20,11 @@ if __name__ == '__main__':
 # end if
 
 
-class Example(object):
+class Example(unittest.TestCase):
     """
     Example for the different types of value retrieval we support for stuff like `title` etc.
     """
+    data: Data = Data()
 
     # noinspection PyMethodMayBeStatic
     def var0(self, data: 'Data') -> str:
@@ -71,8 +60,16 @@ class Example(object):
         return "(8 staticmethod)\nPage " + str(data.button_page)
     # end def
 
-    def get_value_by_name(cls, key):
-        return Menu.get_value_by_name(cls, key)
+    # noinspection PyPropertyDefinition
+    @classproperty
+    def var9(self, data: 'Data') -> str:
+        return "(9 classproperty)\nPage " + str(data.button_page)
+    # end def
+
+    @classmethod
+    def get_value(cls, value):
+        get_value = Menu.get_value.__func__
+        return get_value(cls=cls, value=value)
     # end def
 
     def assertEqual(self, a, b):
@@ -80,301 +77,54 @@ class Example(object):
     # end def
 
     def test_var0(self):
-        data = Data()
-        data.button_page = 123
-        self.assertEqual(self.get_value_by_name('var0'), "(0 normal def)\nPage 123")
+        self.__class__.data = Data()
+        self.__class__.data.button_page = 123
+        self.assertEqual(self.get_value(self.var0), "(0 normal def)\nPage 123")
     # end def
 
     def test_var1(self):
-        data = Data()
-        data.button_page = 123
-        self.assertEqual(self.get_value_by_name('var1'), "(1 normal str)\nPage 123")
+        self.__class__.data = Data()
+        self.__class__.data.button_page = 123
+        self.assertEqual(self.get_value(self.var1), "(1 normal str)\nPage 123")
     # end def
 
-    def test_var2(self):
-        data = Data()
-        data.button_page = 123
-        self.assertEqual(self.get_value_by_name('var2'), "(2 lambda)\nPage 123")
+    @classmethod
+    def test_var2(cls):
+        cls.data.button_page = 123
+        cls.assertEqual(cls, cls.get_value(cls.var2), "(2 lambda)\nPage 123")
     # end def
 
     def test_var3(self):
-        data = Data()
-        data.button_page = 123
-        self.assertEqual(self.get_value_by_name('var3'), "(3 classmethod)\nPage 123")
+        self.assertEqual(self.get_value(self.var3), "(3 classmethod)\nPage 123")
     # end def
 
     def test_var5(self):
-        data = Data()
-        data.button_page = 123
-        self.assertEqual(self.get_value_by_name('var5'), "(5 classmethod)\nPage 123")
+        self.__class__.data = Data()
+        self.__class__.data.button_page = 123
+        self.assertEqual(self.get_value(self.var5), "(5 classmethod)\nPage 123")
     # end def
 
     def test_var6(self):
-        data = Data()
-        data.button_page = 123
-        self.assertEqual(self.get_value_by_name('var6'), "(6 property)\nPage 123")
-    # end def
-# end class
-
-
-# ------------------------------
-# TEST CLASSES
-# here the stuff is implemented
-# ------------------------------
-
-
-teleflask = Teleflask(api_key=API_KEY)
-telemenu = TeleMenuMachine(database_driver=SimpleDictDriver(), teleflask_or_tblueprint=teleflask)
-
-telemenu.states.DEFAULT.on_command('start')
-telemenu.states.DEFAULT.on_command('help')
-def cmd_start(update: Update, text: Union[str, None]):
-    RegisterTestMenu.send()
-# end def
-
-
-@telemenu.register
-class RegisterTestMenu(Menu):
-    # noinspection PyNestedDecorators
-    @TeleMenuMachine.mark_for_register.on_message
-    @classmethod
-    def on_message_listener(cls, update: Update, msg: Message):
-        """
-        Handles callbackdata, registered by
-        :param update:
-        :return:
-        """
-        pass
+        self.__class__.data = Data()
+        self.__class__.data.button_page = 123
+        self.assertEqual(self.__class__.get_value(self.__class__.var6), "(6 property)\nPage 123")
     # end def
 
-    @TeleMenuMachine.mark_for_register.on_command('init')
-    @classmethod
-    def on_command_listener(cls, update: Update, text: Union[str, None]):
-        """
-        Handles callbackdata, registered by
-        :param update:
-        :return:
-        :return:
-        """
-        pass
-    # end def
-# end class
-
-
-@telemenu.register
-class TestMainMenu(GotoMenu):
-    # noinspection PyMethodMayBeStatic
-    def title(self):
-        return "Where to go?"
+    def test_var7(self):
+        self.__class__.data = Data()
+        self.__class__.data.button_page = 123
+        self.assertEqual(self.get_value(self.var7), "(7 str dot format)\nPage 123")
     # end def
 
-    # noinspection PyMethodMayBeStatic
-    def description(self):
-        return "Which text do you want to use?"
+    def test_var8(self):
+        self.__class__.data = Data()
+        self.__class__.data.button_page = 123
+        self.assertEqual(self.get_value(self.var8), "(8 staticmethod)\nPage 123")
     # end def
 
-    # noinspection PyMethodMayBeStatic
-    def menus(self) -> List[GotoButton]:
-        return [
-            GotoButton(label='Checkbox', menu=TestCheckboxMenu, id='something_checkbox'),
-            GotoButton(label='Radio Buttons', menu=TestRadioMenu, id=None),
-            GotoButton(label='Text', menu=TestTextStrMenu),
-            GotoButton(label='Int', menu=TestTextIntMenu),
-            GotoButton(label='Password', menu=TestTextPasswordMenu),
-            GotoButton(label='Email', menu=TestTextEmailMenu),
-            GotoButton(label='Password', menu=TestTextPasswordMenu),
-            GotoButton(label='Upload', menu=TestUploadMenu),
-        ]
+    def test_var9(self):
+        self.__class__.data = Data()
+        self.__class__.data.button_page = 123
+        self.assertEqual(self.get_value(self.var9), "(9 classproperty)\nPage 123")
     # end def
 # end class
-
-
-@telemenu.register
-class TestCheckboxMenu(CheckboxMenu):
-    title = "Shopping list"
-    description = "The shopping list for {now.format('dd.mm.yyyy')!s}"
-
-    @staticmethod
-    def checkboxes(cls: CheckboxMenu) -> List[CheckboxButton]:
-        x: TeleMenuInstancesItem = cls._state_instance
-        s: TeleState = x.state.CURRENT
-        n: str = cls.id
-        s.data = Data(menus={n: MenuData(message_id=123, page=0, )})
-        return [
-            CheckboxButton(title='Eggs', default_selected=True, value='eggs'),
-            CheckboxButton(title='Milk', default_selected=False, value='milk'),
-            CheckboxButton(title='Flux compensator', default_selected=False, value='flux'),
-            CheckboxButton(title='LOVE', default_selected=False, value=None),
-        ]
-    # end def
-# end class
-
-
-@telemenu.register
-class TestRadioMenu(RadioMenu):
-    title = "Best Pony?"
-    description = None
-
-    # noinspection PyMethodMayBeStatic
-    def radiobuttons(self) -> List[RadioButton]:
-        return [
-            RadioButton(title="Applejack", default_selected=False, value='aj'),
-            RadioButton(title="Fluttershy", default_selected=False, value='fs'),
-            RadioButton(title="Rarity", default_selected=False, value='rara'),
-            RadioButton(title="Twilight", default_selected=False, value='ts'),
-            RadioButton(title="Pinkie Pie", default_selected=False, value='pp'),
-            RadioButton(title="Littlepip", default_selected=True, value='waifu'),
-            RadioButton(title="Your Mom", default_selected=False, value='mom'),
-            RadioButton(title="Changelings", default_selected=False, value='bug'),
-            RadioButton(title="Cheesalys", default_selected=False, value='BUG'),
-            RadioButton(title="Your face", default_selected=False, value=':('),
-            RadioButton(title="Spike", default_selected=False, value='just_no'),
-        ]
-    # end def
-# end class
-
-
-@telemenu.register
-class TestTextStrMenu(TextStrMenu):
-    title = None
-    description = "Tell me your name, please."
-
-    done = lambda self: DoneButton(menu=TestTextEmailMenu),
-# end class
-
-
-@telemenu.register
-class TestTextIntMenu(TextIntMenu):
-    title = "Age"
-    description = lambda self: "Tell me your age, please."
-
-    def done(self):
-        return DoneButton(menu=TestTextFloatMenu, label="Next field")
-    # pass
-# end class
-
-
-@telemenu.register
-class TestTextFloatMenu(TextFloatMenu):
-    title = lambda self: "Height"
-    description = "Please tell me your body height in centimeters"
-
-    done = None
-# end class
-
-
-@telemenu.register
-class TestTextPasswordMenu(TextPasswordMenu):
-    title = "Password"
-    description = "Set a password please."
-
-    done = TestTextIntMenu
-# end class
-
-
-@telemenu.register
-class TestTextEmailMenu(TextEmailMenu):
-    title = "Email"
-    description = "Set a email please."
-
-    done = GotoButton(menu=TestTextPasswordMenu, label="Done"),
-# end class
-
-
-@telemenu.register
-class TestTextTelMenu(TextTelMenu):
-    title = "Email"
-    description = "Set a email please."
-# end class
-
-
-@telemenu.register
-class TestTextUrlMenu(TextUrlMenu):
-    title = "URL"
-    description = "What's your website?"
-# end class
-
-
-@telemenu.register
-class TestUploadMenu(UploadMenu):
-    title = "File"
-    description = "Please upload an image."
-    allowed_mime_types = [re.compile(r'image/.*')]
-
-    done = DoneButton(TestMainMenu)
-# end class
-
-class BotMock(object):
-    class BotMockFunc(object): pass
-
-    def __getattr__(self, item):
-        def function(*args, **kwargs):
-            logger.debug(f'MOCK: called {item}(*{args}, **{kwargs})')
-            return None
-        # end def
-        return function
-    # end def
-# end class
-
-
-class UnitTests(unittest.TestCase):
-    def test_state_change(self):
-         telemenu.states.DEFAULT.activate()
-         self.assertEquals(telemenu.states.CURRENT, telemenu.states.DEFAULT, 'should start with default.')
-         TestMainMenu.activate()
-         self.assertNotEquals(telemenu.states.CURRENT, telemenu.states.DEFAULT, 'should not be default any longer.')
-         self.assertEquals(telemenu.states.CURRENT, cast(TeleMenuInstancesItem, TestMainMenu._state_instance).state, 'current state should be state of activated menu.')
-    # end def
-
-    def test_state_change_2(self):
-        telemenu.states.DEFAULT.activate()
-        self.assertEquals(telemenu.states.CURRENT, telemenu.states.DEFAULT, 'should start with default.')
-        telemenu.instances['TEST_MAIN_MENU'].menu.activate()
-        self.assertEqual(telemenu.get_current_menu(), TestMainMenu)
-        self.assertEqual(telemenu.instances[TestMainMenu.id].menu, telemenu.get_current_menu())
-    # end def
-
-    def test_get_property(self):
-        telemenu.states.DEFAULT.activate()
-        self.assertEqual(telemenu.states.CURRENT, telemenu.states.DEFAULT, 'should start with default.')
-        telemenu.instances[TestMainMenu.id].menu.activate()
-        self.assertEqual(telemenu.get_current_menu().get_value_by_name('title'), "Where to go?")
-    # end def
-
-    def test_menus(self):
-        TestRadioMenu.activate()
-        self.assertEqual(telemenu.states.CURRENT.name, 'TEST_RADIO_MENU')
-        self.assertEqual(telemenu.get_current_menu(), TestRadioMenu)
-        self.assertEqual(telemenu.get_current_menu().get_value_by_name('title'),  "Best Pony?")
-        self.assertEqual(telemenu.get_current_menu().title, "Best Pony?")
-    # end def
-
-    def test_history(self):
-        TestCheckboxMenu.activate()
-        self.assertEqual(telemenu.get_current_menu().data.history, ['TEST_MAIN_MENU', 'TEST_CHECKBOX_MENU'])
-        self.assertEqual(telemenu.get_current_menu(), TestCheckboxMenu)
-        self.assertEqual(telemenu.get_last_menu(), TestMainMenu)
-        telemenu.get_last_menu(activate=True)
-        self.assertEqual(telemenu.get_current_menu().data.history, ['TEST_MAIN_MENU'])
-        self.assertEqual(
-            telemenu.get_current_menu().get_value(telemenu.get_current_menu().text),
-            telemenu.get_current_menu().get_value_by_name('text')
-        )
-    # end def
-
-    def test_reply_markup(self):
-        TestTextUrlMenu.activate()
-        menu = telemenu.get_current_menu()
-        self.assertIsInstance(menu.reply_markup(), ForceReply)
-    # end def
-# end class
-
-from telestate import TeleStateMachine
-
-# teleflask.process_update(Update(123, Message(456, 0, Chat(-123, 'private'))))
-
-teleflask.process_update(Update(123, Message(456, 0, Chat(-123, 'private'), text="/cancel")))
-teleflask.process_update(Update(123, Message(456, 0, Chat(-123, 'private'), text="/start wowsa")))
-
-# menu = telemenu.get_current_menu()
-# menu.send()
